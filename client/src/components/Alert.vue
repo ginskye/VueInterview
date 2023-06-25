90ed8f0eea9b406c6fd32bb4d9cde25949f93664
Emily Hall <bibliomaniac@gmail.com>
2023-06-25 13:19:38 -0400

    add put/edit paths

---
+++ client/src/components/Alert.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <div class="alert alert-success" role="alert">{{ message }}
+            <!-- <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button> -->
+</div>
+        <br/>
+    </div>
+</template>
+<script>
+export default {
+    props: ['message'],
+};
+</script>
